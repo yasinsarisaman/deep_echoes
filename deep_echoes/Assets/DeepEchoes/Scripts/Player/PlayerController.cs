2f49f32cfeeab43e6f9c71173afbc072f5b0194f
yasinsarisaman <yasinsarisaman@firmadefteri.com>
2024-03-30 16:58:11 +0300

    (Developement) : Isometric Movement : Player controller system added.

---
+++ deep_echoes/Assets/DeepEchoes/Scripts/Player/PlayerController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rbody;
+    [SerializeField] private float speed;
+    [SerializeField] private float turnSpeed;
+    private Vector3 movementInput;
+
+    private void Update()
+    {
+        Look();
+    }
+
+
+    public void FixedUpdate()
+    {
+        Move();
+        //movementInput = Vector3.zero;
+    }
+
+    /* Runs when the any of movement inputs triggered */
+    public void MovementTrigger(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        movementInput = new Vector3(input.x, 0, input.y);
+        
+    }
+
+    public void Look()
+    {
+        if (movementInput != Vector3.zero)
+        {
+            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+            var skewedInput = matrix.MultiplyPoint3x4(movementInput);
+            var relative = (transform.position + skewedInput) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Move()
+    {
+        rbody.MovePosition(transform.position + transform.forward * (movementInput.magnitude * speed * Time.deltaTime));
+    }
+}
