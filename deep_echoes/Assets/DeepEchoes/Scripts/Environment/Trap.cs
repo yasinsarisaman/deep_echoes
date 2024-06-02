643f819fb8a89f1169f9d6a9616630b762701060
yasinsarisaman <yasinsarisaman@firmadefteri.com>
2024-06-02 19:32:56 +0300

    Feat : (SFX) Added wrench and trap collision sounds.

--- deep_echoes/Assets/DeepEchoes/Scripts/Environment/Trap.cs
+++ deep_echoes/Assets/DeepEchoes/Scripts/Environment/Trap.cs
@@ -6,12 +6,21 @@ namespace DeepEchoes.Scripts.Environment
     public class Trap : Damagable
     {
         [SerializeField] private int _value;
+        [SerializeField] private AudioSource _audioSource;
         
         protected override void ApplyDamage()
         {
             EventBus<ApplyDamageEvent>.Emit(this, new ApplyDamageEvent(_value));
+            PlayColisionSound();
             base.ApplyDamage();
         }
         
+        public void PlayColisionSound()
+        {
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
+        }
     }
 }
